ea5d5247ae09787d8d26fc0c30a4108f9f40d642
Busra Gazel <busragzl96@gmail.com>
2024-12-21 00:27:21 +0300

    Initial Commit

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by Musa on 21.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
